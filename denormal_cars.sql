98b785ab5d834a75c88e50e2a93b1d2ad07c621c
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-20 18:34:10 -1000

    select distinct

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -10,6 +10,13 @@ CREATE DATABASE denormal_cars OWNER denormal_user;
 
 \i Denormal-Automotives/scripts/denormal_data.sql
 
-SELECT DISTINCT count(make_title)
-FROM car_models;
+-- SELECT DISTINCT count(make_title)
+-- FROM car_models;
 
+-- SELECT DISTINCT count(model_title)
+-- FROM car_models
+-- WHERE make_code = 'VOLKS';
+
+-- SELECT DISTINCT make_code, model_code, model_title, year
+-- FROM car_models
+-- WHERE make_code = 'LAM';
