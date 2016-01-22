bc78bed202b9f3c44a2f25374cb5f8a3c09c796a
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-21 17:40:50 -1000

    update

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,10 +1,28 @@
-DROP USER IF EXISTS normal_user;
+DROP DATABASE IF EXISTS "normal_car";
+
+DROP USER IF EXISTS "normal_user";
 
 CREATE USER normal_user;
 
-DROP DATABASE IF EXISTS normal_car;
-
 CREATE DATABASE normal_car OWNER normal_user;
+
+
 
 \c normal_car;
 
+\i Denormal-Automotives/scripts/denormal_data.sql;
+
+\dt;
+
+-- ALTER TABLE makers(
+-- id serial,
+-- PRIMARY KEY ("id"),
+-- -- make_code character varying(125) NOT NULL,
+-- make_title character varying(125) NOT NULL
+-- );
+
+-- INSERT INTO makers(make_title)
+-- VALUES(
+-- SELECT DISTINCT make_title
+-- FROM car_models
+-- );
